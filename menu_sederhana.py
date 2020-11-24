1bce1ed73dcb16c488d4d2abb96ec6f4dbf57b46
Pandhu Brian Aldhiansyah <60566238+pandhuba@users.noreply.github.com>
2020-11-24 14:58:40 +0700

    Update code
    Removing 3 function unused in hitungAngka class

--- menu_sederhana.py
+++ menu_sederhana.py
@@ -1,10 +1,4 @@
 class hitungAngka:
-    def tambah(a,b):
-        return a+b
-    def kurang(a,b):
-        return a-b
-    def kali(a,b):
-        return a*b
     def bagi(a,b):
         return float(a/b)
     def modulus(a,b):
